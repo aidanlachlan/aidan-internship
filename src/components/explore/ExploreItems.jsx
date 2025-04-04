e7d031c4db5da946eda927129bb52963e947355b
Aidan <aidanlmcmurray@gmail.com>
2025-04-04 09:33:56 -0700

    reusable cards

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import Timer from "../Timer";
 import ExploreItemsSkeleton from "../ExploreItemsSkeleton";
+import NftCard from "../NftCard";
 
 const ExploreItems = () => {
   const [exploreItems, setExploreItems] = useState([]);
@@ -26,58 +26,7 @@ const ExploreItems = () => {
           className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
           style={{ display: "block", backgroundSize: "cover" }}
         >
-          <div className="nft__item">
-            <div className="author_list_pp">
-              <Link
-                to={`/author/${item.authorId}`}
-                data-bs-toggle="tooltip"
-                data-bs-placement="top"
-              >
-                <img className="lazy" src={item.authorImage} alt="" />
-                <i className="fa fa-check"></i>
-              </Link>
-            </div>
-            <div className="de_countdown">
-              <Timer duration={item.expiryDate} />
-            </div>
-
-            <div className="nft__item_wrap">
-              <div className="nft__item_extra">
-                <div className="nft__item_buttons">
-                  <button>Buy Now</button>
-                  <div className="nft__item_share">
-                    <h4>Share</h4>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-facebook fa-lg"></i>
-                    </a>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-twitter fa-lg"></i>
-                    </a>
-                    <a href="">
-                      <i className="fa fa-envelope fa-lg"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <Link to={`/item-details/${item.nftId}`}>
-                <img
-                  src={item.nftImage}
-                  className="lazy nft__item_preview"
-                  alt=""
-                />
-              </Link>
-            </div>
-            <div className="nft__item_info">
-              <Link to={`/item-details/${item.nftId}`}>
-                <h4>{item.title}</h4>
-              </Link>
-              <div className="nft__item_price">{item.price} ETH</div>
-              <div className="nft__item_like">
-                <i className="fa fa-heart"></i>
-                <span>{item.likes}</span>
-              </div>
-            </div>
-          </div>
+          <NftCard item={item} />
         </div>
       );
     });
@@ -85,10 +34,10 @@ const ExploreItems = () => {
 
   function renderExploreItemsSkeleton() {
     return Array(16)
-    .fill(0)
-    .map((_, i) => {
-      return <ExploreItemsSkeleton key={i} />;
-    });
+      .fill(0)
+      .map((_, i) => {
+        return <ExploreItemsSkeleton key={i} />;
+      });
   }
 
   useEffect(() => {
